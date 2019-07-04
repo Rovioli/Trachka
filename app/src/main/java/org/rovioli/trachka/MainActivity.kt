0bb3ec9ae2d3d021128ebde836a47548ace1594d
Vitalii Dmitriev <dmitvitalii@gmail.com>
2019-07-04 21:44:47 +0300

    Add try again and refactor a bit

--- app/src/main/java/org/rovioli/trachka/MainActivity.kt
+++ app/src/main/java/org/rovioli/trachka/MainActivity.kt
@@ -1,5 +1,6 @@
 package org.rovioli.trachka
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
@@ -16,34 +17,46 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val service = Connector.client
-        val context = this
-
-        GlobalScope.launch(Dispatchers.Main) {
-            try {
-                val response = service.getUsers()
-                val users = response.body()?.data
-                val names = users?.map { it.name }
-                usernameSpinner.adapter = ArrayAdapter(
-                    context, android.R.layout.simple_spinner_dropdown_item, names
-                )
-                usernameSpinner.visibility = View.VISIBLE
-                login.visibility = View.VISIBLE
-                progressBar.visibility = View.GONE
-            } catch (e: Throwable) {
-                e.printStackTrace()
-                Toast.makeText(context, "Connection Error", Toast.LENGTH_LONG).show()
-                progressBar.visibility = View.GONE
-            }
-        }
-
         val intent = Intent(this, UserActivity::class.java)
-
+        tryAgain.setOnClickListener { attemptShowContent() }
         login.setOnClickListener {
             intent.putExtra("name", usernameSpinner.selectedItem.toString())
             val userId = usernameSpinner.selectedItemId + 1
             intent.putExtra("id", userId.toInt())
             startActivity(intent)
         }
+
+        attemptShowContent()
+    }
+
+    private fun attemptShowContent() = GlobalScope.launch(Dispatchers.Main) {
+        try {
+            showContent(this@MainActivity)
+        } catch (e: Throwable) {
+            e.printStackTrace()
+            showError(this@MainActivity)
+        }
+    }
+
+    private fun showError(context: Context) {
+        Toast.makeText(context, "Connection Error", Toast.LENGTH_LONG).show()
+        progressBar.visibility = View.INVISIBLE
+        tryAgain.visibility = View.VISIBLE
+    }
+
+    private suspend fun showContent(context: Context) {
+        tryAgain.visibility = View.INVISIBLE
+        progressBar.visibility = View.VISIBLE
+
+        val response = Connector.client.getUsers()
+        val users = response.body()?.data
+        val names = users?.map { it.name }
+        usernameSpinner.adapter = ArrayAdapter(
+            context, android.R.layout.simple_spinner_dropdown_item, names
+        )
+
+        usernameSpinner.visibility = View.VISIBLE
+        login.visibility = View.VISIBLE
+        progressBar.visibility = View.INVISIBLE
     }
 }
