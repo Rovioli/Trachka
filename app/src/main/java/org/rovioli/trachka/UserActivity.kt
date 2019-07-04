633b053892e5f385f841fabe41f6f801b1ccaf6e
Vitalii Dmitriev <dmitvitalii@gmail.com>
2019-07-04 23:24:54 +0300

    Sync ViewPAger and bottonthing

--- app/src/main/java/org/rovioli/trachka/UserActivity.kt
+++ app/src/main/java/org/rovioli/trachka/UserActivity.kt
@@ -2,6 +2,7 @@ package org.rovioli.trachka
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.viewpager.widget.ViewPager
 import kotlinx.android.synthetic.main.activity_user.*
 
 class UserActivity : AppCompatActivity() {
@@ -10,6 +11,18 @@ class UserActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user)
         viewPager.adapter = MainPagerAdapter(supportFragmentManager)
+        viewPager.addOnPageChangeListener(object : ViewPager.OnPageChangeListener {
+            override fun onPageScrollStateChanged(state: Int) {
+            }
+
+            override fun onPageScrolled(position: Int, positionOffset: Float, positionOffsetPixels: Int) {
+            }
+
+            override fun onPageSelected(position: Int) {
+                navView.menu.getItem(position).isChecked = true
+            }
+
+        })
         navView.setOnNavigationItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> {
